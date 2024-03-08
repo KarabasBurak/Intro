7069995d9441b95e27f27182deefba62b5629649
KarabasBurak <117377125+KarabasBurak@users.noreply.github.com>
2024-03-08 14:04:38 +0300

    Add project files.

---
+++ Intro/Business/CourseManager.cs
@@ -0,0 +1,44 @@
+﻿using Intro.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intro.Business;
+
+public class CourseManager
+{
+    Course[] courses=new Course[3];
+
+    public CourseManager()
+    {
+        Course course = new Course();
+        course.Id = 1;
+        course.Name = "C#";
+        course.Description = ".Net 8";
+        course.Price = 0;
+
+        Course course1 = new Course();
+        course1.Id = 2;
+        course1.Name = "JAVA";
+        course1.Description = "JAVA 17";
+        course1.Price = 10;
+
+        Course course2 = new Course();
+        course2.Id = 3;
+        course2.Name = "Python";
+        course2.Description = "Python 3.12";
+        course2.Price = 20;
+
+        courses[0]= course;
+        courses[1]= course1;
+        courses[2]= course2;
+
+    }
+    public Course[] GetAll()
+    {
+        return courses;
+    }
+}
---
+++ Intro/Program.cs
@@ -0,0 +1,86 @@
+﻿
+using Intro.Business;
+using Intro.Entities;
+
+Console.WriteLine("Hello, World!");
+
+string message1 = "Krediler";
+int term = 12;
+double amount = 10000;
+
+bool isAuthenticated = false;
+Console.WriteLine(message1);
+
+// condition
+if (isAuthenticated)
+{
+    Console.WriteLine("Hoşgeldin Burak");
+}
+else
+{
+    Console.WriteLine("Sisteme Giriş Yap");
+}
+
+string[] loans = { "Kredi1", "Kredi2", "Kredi3", "Kredi4", "Kredi5" };
+
+// Start
+for (int i = 0; i < loans.Length; i++)
+{
+    Console.Write(loans[i]);
+}
+
+CourseManager courseManager = new();
+Course[] courses2 = courseManager.GetAll();
+for (int i = 0; i < courses2.Length; i++)
+{
+    Console.WriteLine(courses2[i].Name + " / " + courses2[i].Price);
+
+}
+
+
+IndividualCustomer customer = new IndividualCustomer();
+customer.Id = 1;
+customer.NationalIdentity = "26405710245";
+customer.FirstName = "Burak";
+customer.LastName = "KARABAŞ";
+customer.CustomerNumber = "123456";
+
+IndividualCustomer customer1 = new IndividualCustomer();
+customer1.Id = 2;
+customer1.NationalIdentity = "26405710287";
+customer1.FirstName = "Özgür";
+customer1.LastName = "Atılgan";
+customer1.CustomerNumber = "123459";
+
+CorporateCustomer corporateCustomer = new CorporateCustomer();
+corporateCustomer.Id = 3;
+corporateCustomer.Name = "Kodlamaio";
+corporateCustomer.CustomerNumber = "654778";
+corporateCustomer.TaxIdentity = "12345678987";
+
+CorporateCustomer corporateCustomer1 = new CorporateCustomer();
+corporateCustomer1.Id = 4;
+corporateCustomer1.Name = "Repkon";
+corporateCustomer1.CustomerNumber = "654779";
+corporateCustomer1.TaxIdentity = "12345678990";
+
+int number1 = 10;
+int number2 = 20;
+number1 = number2;
+number2 = 50;
+Console.WriteLine(number1);
+
+string[] cities1 = { "Ankara", "İstanbul", "İzmir" };
+string[] cities2 = { "Bursa", "Bolu", "Diyarbakır" };
+
+cities2 = cities1;
+cities1[0] = "Adana";
+
+Console.WriteLine(cities2[0]);
+
+BaseCustomer[] customers = { customer, customer1, corporateCustomer, corporateCustomer1 };
+
+foreach (BaseCustomer model in customers)
+{
+    Console.WriteLine(model.CustomerNumber);
+}
